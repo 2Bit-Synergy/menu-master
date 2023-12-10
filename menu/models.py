cf4f44494afd2d9f49421de615913feebec926ad
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-10 22:13:56 +0800

    Update Item migration

--- menu/models.py
+++ menu/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 class Item(models.Model):
+    
+    def __str__(self):
+        return self.name
+    
+    
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     price = models.IntegerField()
+    image = models.CharField(max_length=500, default="http://www.energyfit.com.mk/wp-content/plugins/ap_background/images/default/default_1.png")
+    
