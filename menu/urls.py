a319f0344635ee1e97a38131b99b610bb01f1896
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-11 16:25:08 +0800

    Implement remaining CRUD actions

--- menu/urls.py
+++ menu/urls.py
@@ -6,6 +6,8 @@ app_name = 'menu'
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('menu/<int:id>', views.detail, name="detail"),
+    path('menu/<int:id>/', views.detail, name="detail"),
     path('menu/create', views.create, name="create"),
+    path('menu/update/<int:id>/', views.update, name="update"),
+    path('menu/delete/<int:id>/', views.delete, name="delete"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -23,3 +23,24 @@ def create(request):
         return redirect('menu:index')
 
     return render(request, 'create.html', { 'form': form })
+
+def update(request, id):
+    item = Item.objects.get(pk=id)
+    
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('menu:index')
+
+    return render(request, 'update.html', { 'form': form, 'item': item })
+
+def delete(request, id):
+    item = Item.objects.get(pk=id)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('menu:index')
+
+    return render(request, 'delete.html', { 'item': item })
+
