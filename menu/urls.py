5910f8f44c61adba2900d3b63a63d180feb059fd
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-11 15:34:22 +0800

    Implement add item

--- menu/urls.py
+++ menu/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+# Set namespace for our menu app
+app_name = 'menu'
+
 urlpatterns = [
-    path('', views.index, name="menu:index"),
-    path('menu/<int:id>', views.detail, name="menu:detail"),
-    path('menu/create', views.create, name="menu:create"),
+    path('', views.index, name="index"),
+    path('menu/<int:id>', views.detail, name="detail"),
+    path('menu/create', views.create, name="create"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Item
+from .forms import ItemForm
 
 def index(request):
-    return render(request, 'index.html')
+    items = Item.objects.all()
+    
+    return render(request, 'index.html', { 'items': items })
 
 def detail(request, id):
-    return render(request, 'detail.html', { 'id': id })
+    item = Item.objects.get(pk=id)
+    context = {
+        'item': item
+    }
+    return render(request, 'detail.html', context)
 
 def create(request):
-    return render(request, 'create.html')
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('menu:index')
+
+    return render(request, 'create.html', { 'form': form })
