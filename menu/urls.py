ec279d7277a7494f61680d61a9506a53f0d95801
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-11 09:32:15 +0800

    Create a basic routing and refactor layouts

--- menu/urls.py
+++ menu/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="menu:index"),
+    path('<int:id>', views.detail, name="menu:detail"),
+    path('create', views.create, name="menu:create"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html');
+    return render(request, 'index.html')
+
+def detail(request):
+    return render(request, 'detail.html')
+
+def create(request):
+    return render(request, 'create.html')
