f7041b77739ea7212bccdda3637115ac03150b97
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-13 09:04:33 +0800

    Associate user_id with authenticated user

--- menu/urls.py
+++ menu/urls.py
@@ -7,7 +7,7 @@ app_name = 'menu'
 urlpatterns = [
     path('', views.IndexClassView.as_view(), name="index"),
     path('menu/<int:pk>/', views.DetailClassView.as_view(), name="detail"),
-    path('menu/create', views.create, name="create"),
+    path('menu/create', views.CreateClassView.as_view(), name="create"),
     path('menu/update/<int:id>/', views.update, name="update"),
     path('menu/delete/<int:id>/', views.delete, name="delete"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,10 @@
+from django.forms.models import BaseModelForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Item
 from .forms import ItemForm
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -16,15 +18,15 @@ class DetailClassView(LoginRequiredMixin, DetailView):
     model = Item
     template_name = 'detail.html'
 
-@login_required
-def create(request):
-    form = ItemForm(request.POST or None)
-
-    if form.is_valid():
-        form.save()
-        return redirect('menu:index')
-
-    return render(request, 'create.html', { 'form': form })
+class CreateClassView(LoginRequiredMixin, CreateView):
+    model = Item
+    fields = ['name', 'description', 'price', 'image']
+    template_name = 'create.html' 
+    
+    def form_valid(self, form):
+        form.instance.user_id = self.request.user
+        
+        return super().form_valid(form)
 
 @login_required
 def update(request, id):
